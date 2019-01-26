21a1178113a6075e24a36e27f360912df6d709ad
Skylar McCain <skylarmccain@gmail.com>
2019-01-26 12:05:24 -0500

    give up

--- main.cpp
+++ main.cpp
@@ -20,32 +20,42 @@ struct Product
 
 struct Drone 
 {
-    intp pair;
+    intp currentLoc;
     int maxPayLoad;
     vector<product> products; // same size as wareHouse product vecotr
 
     Drone(int maxP)
     {
         maxPayLoad = maxP;
+        currentLoc.first = 0;
+        currentLoc.second = 0;
     }
 
 
     //items is a vector of amount of items to take from each product ; should be the same size as w
     //d = GetDistance(droneLocation, wareHouseLocation)
-    Load(double d, WareHouse& w, Vector& items) 
+    Load(WareHouse& w, Vector& items) 
     {
+        d = GetDistance(currentLoc, w.warLoc)
         for(unsigned int i = 0; i < items.Size(); i++)
         {  
             w.listProdType[1] = w.listProdType[i] - items[i];
             products[i] = products[i] + items[i];
             maxPayLoad = maxPayLoad + (items[i] * items[i].weight);
         }
-        totalTurns  = totalTurns +  d + 1;
+        totalTurns  = totalTurns - d - 1;
     }
 
-    Deliver(double d, Order& o)
+    //must drop of ALL items 
+    Deliver(intp DeliverLoc, Order& o, Vector& itmes)
     {
-        
+        double d = GetDistance(currentLoc, DeliverLoc);
+        for(unsigned int i = 0; i < o.size(), i++)
+        {
+            order[i] = order[i] - items[i];
+            maxPayLoad = maxPayload - (items[i] * items[i].weight);
+        }
+        totalTurns  = totalTurns -  d - 1;
 
     }
 
@@ -58,11 +68,6 @@ struct Drone
     {
 
     }
-
-};
-
-struct Location
-{
 
 };
 
