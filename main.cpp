e9438dda30bac0e24e122b7056787ebd393507bd
Amar <amarsgithub@outlook.com>
2019-01-26 08:37:06 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+typedef unsigned int uint;
+typedef pair<int,int> intp;
+
+
+struct Product
+{
+
+};
+
+struct Drone 
+{
+
+};
+
+
+struct Solution 
+{
+    int rows, cols, drones, turns, maxPayLoad;
+
+
+
+
+};
+
+// EMMETT PLZ WRITE THE FOLLOWING FUNCTION TO CALCULATE DISTANCE BETWEEN 2 POINTS. RETURN DOUBLE.
+double GetDistance(intp one, intp two)
+{
+    double returnValue;
+    
+    return returnValue
+}
+
+
+int main(void)
+{
+    // File IO shit goes here 
+    // All of the files would ideally go into a vector of ifstream objects. 
+    ifstream input();
+
+    // Create a solution vector as well that takes in all files 
+    
+
+
+    return 0;
+}
